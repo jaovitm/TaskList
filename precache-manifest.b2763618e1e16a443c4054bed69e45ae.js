77963aa9fbd25a037d0a5d127a5019963466dacd
Joao Vitor Machado <joaovitormdesa@gmail.com>
2022-06-06 21:26:36 -0300

    Updates

---
+++ precache-manifest.b2763618e1e16a443c4054bed69e45ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbdc9709f604ea4825b4",
+    "url": "/TaskList/static/css/main.3edeb0ad.chunk.css"
+  },
+  {
+    "revision": "cbdc9709f604ea4825b4",
+    "url": "/TaskList/static/js/main.cbdc9709.chunk.js"
+  },
+  {
+    "revision": "5084bace1d8e4dd84a3d",
+    "url": "/TaskList/static/js/1.5084bace.chunk.js"
+  },
+  {
+    "revision": "b959a96c6f9e825fab5a",
+    "url": "/TaskList/static/js/runtime~main.b959a96c.js"
+  },
+  {
+    "revision": "6f9fbbc4fb60699b32e2f2489545de5c",
+    "url": "/TaskList/index.html"
+  }
+];
--- src/components/header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const header = () => {
-    return ( 
-        <h1 style={{color:'#eee'}}>Minhas Tarefas</h1>
-     );
-}
- 
-export default header;
